340cf1922f7fa85860219d6f8c72bd0cc255d16e
jukmali <54353202+jukmali@users.noreply.github.com>
2022-08-29 09:46:06 +0300

    Update shopping.js

--- js/shopping.js
+++ js/shopping.js
@@ -3,6 +3,8 @@
 
 // Function called when the form is submitted.
 // Function performs the calculation and returns false.
+
+// Exercise: 3
 function calculate() {
     'use strict';
 
@@ -10,11 +12,12 @@ function calculate() {
     var total;
 
     // Get references to the form values:
-    var quantity = document.getElementById('quantity') ;
+    var quantity = document.getElementById('quantity').value ; // add value
 
     var price =  document.getElementById('price').value ;
-    var tax =  document.getElementById('tax') ;
-    var discount =  document.getElementById('discount') ;
+    var tax =  document.getElementById('tax').value ; // add value
+    var discount =  document.getElementById('discount').value ; //add value
+	var shipping = document.getElementById('shipping').value; // add shipping
 
     // Add validation here later!
 
@@ -31,7 +34,13 @@ function calculate() {
     console.log("total after tax: " + total);
 
     // Factor in the discount:
-    total = total - discount;
+	if (quantity > 100){
+		total = total - (2*discount);
+	} else {
+		
+		total = total - discount;
+	}
+	total = total + (1.0*shipping);
     console.log("total after discount: " + total);
 
     // Format the total to two decimal places:
@@ -47,14 +56,22 @@ function calculate() {
 
 // Function called when the window has been loaded.
 // Function needs to add an event listener to the form.
+// Exercise: 2 using eventListener
 function init() {
     'use strict';
 
     // Add an event listener to the form:
+   
     var theForm = document.getElementById('theForm');
-    theForm.onsubmit = calculate;
+    theForm.addEventListener("submit",function(e){
+        if(!calculateMembership()){
+            // Prevent to upload the page 
+            e.preventDefault();
+        }
+    });
+}
 
-} // End of init() function.
+// End of init() function.
 
 // Assign an event listener to the window's load event:
 window.onload = init;
